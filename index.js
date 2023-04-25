913089c4010bd02e495fe57ef3de2e690417b4c7
Harvey <83501308+harveydowning@users.noreply.github.com>
2023-04-25 12:34:42 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function getMilk(money) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+    AmountOfBottles = Math.floor(money / 1.5)
+    Remainder = money % 1.5
+}
+
+getMilk(40)
